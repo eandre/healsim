4f8e1476163e47524597c9832cee56cf4f2819d7
André Eriksson <eandre@gmail.com>
2015-04-12 17:07:37 +0200

    WIP

--- src/healsim/main.rs
+++ src/healsim/main.rs
@@ -1,6 +1,12 @@
 extern crate simlib;
 
 fn main() {
-	let events : Vec<simlib::Event> = vec!(simlib::Event{name: "hi"}, simlib::Event{name: "there"});
-	simlib::moo(events.iter());
+	let events = vec!(simlib::Event{name: "hi"}, simlib::Event{name: "there"});
+	{
+		let iter = &mut events.iter();
+		let sim_stream = simlib::sim(iter);
+		for event in sim_stream {
+			println!("Got event {}", event.name)
+		}
+	}
 }
--- src/simlib/lib.rs
+++ src/simlib/lib.rs
@@ -4,11 +4,21 @@ pub struct Event<'a> {
 	pub name: &'a str
 }
 
-pub fn moo<'a, I>(events: I)
+pub struct SimStream<'a> {
+	source: &'a mut Iterator<Item=&'a Event<'a>>
+}
+
+impl<'a> Iterator for SimStream<'a> {
+	type Item = &'a Event<'a>;
+
+	fn next(&mut self) -> Option<&'a Event<'a>> {
+		self.source.next()
+	}
+}
+
+pub fn sim<'a, I>(events: &'a mut I) -> SimStream<'a>
 	where I: Iterator<Item=&'a Event<'a>>
 {
-	for x in events {
-		println!("event name: {}", x.name);
-	}
+	SimStream{source: events}
 }
 
